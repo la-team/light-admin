f835536f832e8dab5e0387779f63bb3ff965b773
max <kharchenko.max@gmail.com>
2012-10-05 00:04:01 +0300

    Refactoring Attribute Renderer functionality.

--- lightadmin-core/src/main/java/org/lightadmin/core/web/tags/AttributeRendererTag.java
+++ lightadmin-core/src/main/java/org/lightadmin/core/web/tags/AttributeRendererTag.java
@@ -1,17 +1,11 @@
 package org.lightadmin.core.web.tags;
 
 import org.lightadmin.core.persistence.metamodel.DomainTypeAttributeMetadata;
-import org.lightadmin.core.view.renderer.AttributeRenderer;
-import org.lightadmin.core.view.renderer.AttributeRendererFactory;
-import org.lightadmin.core.view.renderer.BasicAttributeRendererFactory;
-import org.springframework.context.ApplicationContext;
-import org.springframework.web.context.support.WebApplicationContextUtils;
-import org.springframework.web.servlet.tags.RequestContextAwareTag;
 
 import javax.servlet.jsp.JspException;
 import java.io.IOException;
 
-public class AttributeRendererTag extends RequestContextAwareTag {
+public class AttributeRendererTag extends AttributeRendererFactoryAwareTag {
 
 	private DomainTypeAttributeMetadata attributeMetadata;
 
@@ -19,19 +13,9 @@ public class AttributeRendererTag extends RequestContextAwareTag {
 
 	@Override
 	protected final int doStartTagInternal() throws JspException, IOException {
-		final AttributeRendererFactory rendererFactory = attributeRendererFactory();
-
-		final AttributeRenderer renderer = rendererFactory.getRenderer( attributeMetadata );
-
-		renderer.render( attributeMetadata, entity, pageContext );
+		rendererFor( attributeMetadata ).render( attributeMetadata, entity, pageContext );
 
 		return SKIP_BODY;
-	}
-
-	private BasicAttributeRendererFactory attributeRendererFactory() {
-		final ApplicationContext applicationContext = WebApplicationContextUtils.getWebApplicationContext( pageContext.getServletContext() );
-
-		return applicationContext.getBean( BasicAttributeRendererFactory.class );
 	}
 
 	public DomainTypeAttributeMetadata getAttributeMetadata() {
