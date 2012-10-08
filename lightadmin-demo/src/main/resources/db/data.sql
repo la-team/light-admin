cb85462e7ce67ece1deef9e39c59325ac9feb6d6
max <kharchenko.max@gmail.com>
2012-10-08 18:45:38 +0300

    DB pre-population

--- lightadmin-demo/src/main/resources/db/data.sql
+++ lightadmin-demo/src/main/resources/db/data.sql
@@ -33,6 +33,6 @@ insert into Product (id, name, description, price) values (3, 'Dock', 'Dock for 
 insert into Product_Attributes (attributes_key, product_id, attributes) values ('connector', 1, 'socket');
 insert into Product_Attributes (attributes_key, product_id, attributes) values ('connector', 3, 'plug');
 
-insert into Orders (id, customer_id, shippingaddress_id) values (1, 1, 2);
+insert into Orders (id, customer_id, billingaddress_id, shippingaddress_id) values (1, 1, 1, 2);
 insert into LineItem (id, product_id, amount, order_id, price) values (1, 1, 2, 1, 499.0);
 insert into LineItem (id, product_id, amount, order_id, price) values (2, 2, 1, 1, 1299.0);
