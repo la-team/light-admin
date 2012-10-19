ada0b8676d8c59af45aa7ad4b8b3802b01a48ec8
max <kharchenko.max@gmail.com>
2012-10-19 23:18:12 +0300

    Unit & Integration tests POM changes

---
+++ lightadmin-core/src/test/java/org/lightadmin/core/LightUnitTest.java
@@ -0,0 +1,13 @@
+package org.lightadmin.core;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class LightUnitTest {
+
+	@Test
+	public void testSmth() {
+		assertTrue(true);
+	}
+}
