c7342e83bfd58ad8835ac507d6baf608187af349
max <kharchenko.max@gmail.com>
2012-11-16 21:00:39 +0200

    DomainConfigurationUnitType unit test updated.

--- lightadmin-core/src/test/java/org/lightadmin/core/config/bootstrap/parsing/configuration/DomainConfigurationUnitTypeTest.java
+++ lightadmin-core/src/test/java/org/lightadmin/core/config/bootstrap/parsing/configuration/DomainConfigurationUnitTypeTest.java
@@ -2,6 +2,10 @@ package org.lightadmin.core.config.bootstrap.parsing.configuration;
 
 import org.junit.Test;
 
+import java.util.Set;
+
+import static com.google.common.collect.Sets.newLinkedHashSet;
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
 public class DomainConfigurationUnitTypeTest {
@@ -14,5 +18,22 @@ public class DomainConfigurationUnitTypeTest {
 	@Test
 	public void findConfigurationUnitByName() throws Exception {
 		assertEquals( DomainConfigurationUnitType.SCREEN_CONTEXT, DomainConfigurationUnitType.forName( "screenContext" ) );
+	}
+
+	@Test
+	public void definedUnitTypeNames() throws Exception {
+		final String[] expectedUnitTypeNames = new String[] {
+			"screenContext", "configuration", "listView", "scopes", "filters"
+		};
+
+		assertArrayEquals( expectedUnitTypeNames, unitTypeNames( DomainConfigurationUnitType.values() ) );
+	}
+
+	private String[] unitTypeNames( DomainConfigurationUnitType[] unitTypes ) {
+		final Set<String> unitTypeNames = newLinkedHashSet();
+		for ( DomainConfigurationUnitType unitType : unitTypes ) {
+			unitTypeNames.add( unitType.getName() );
+		}
+		return unitTypeNames.toArray( new String[unitTypeNames.size()] );
 	}
 }
