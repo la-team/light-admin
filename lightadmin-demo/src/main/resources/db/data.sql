815604ae6ee7009d50056f7658ee12379cb4b7e8
ikostenko <iryna.kostenko@gmail.com>
2012-11-18 19:55:43 +0200

    lineItem data restrored

--- lightadmin-demo/src/main/resources/db/data.sql
+++ lightadmin-demo/src/main/resources/db/data.sql
@@ -36,10 +36,8 @@ insert into Product_Attributes (attributes_key, product_id, attributes) values (
 
 insert into Orders (id, customer_id, billingaddress_id, shippingaddress_id) values (1, 1, 1, 2);
 insert into Orders (id, customer_id, billingaddress_id, shippingaddress_id) values (2, 3, 2, 1);
-insert into Orders (id, customer_id, billingaddress_id, shippingaddress_id) values (3, 25, 2, 1);
-insert into LineItem (id, product_id, amount, order_id) values (1, 1, 120, 1);
-insert into LineItem (id, product_id, amount, order_id) values (2, 2, 1400, 1);
-insert into LineItem (id, product_id, amount, order_id) values (3, 3, 100, 2);
+insert into LineItem (id, product_id, amount, order_id, price) values (1, 1, 2, 1, 499.0);
+insert into LineItem (id, product_id, amount, order_id, price) values (2, 2, 1, 1, 1299.0);
 
 insert into FilterTestEntity (id, textfield, integerfield, decimalfield) values (1, 'integer search test', 1234567, 22.2);
 insert into FilterTestEntity (id, textfield, integerfield, decimalfield) values (2, 'decimal search test', 456, 1499.99);
