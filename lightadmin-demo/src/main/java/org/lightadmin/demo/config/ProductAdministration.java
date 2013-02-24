6049937eca7249bae527f48519099f78144df901
max <kharchenko.max@gmail.com>
2013-02-25 01:18:27 +0200

    #31 Date field registered everywhere in ProductConfiguration except Filters section.

--- lightadmin-demo/src/main/java/org/lightadmin/demo/config/ProductAdministration.java
+++ lightadmin-demo/src/main/java/org/lightadmin/demo/config/ProductAdministration.java
@@ -12,8 +12,8 @@ import org.lightadmin.core.config.domain.filter.FiltersConfigurationUnitBuilder;
 import org.lightadmin.core.config.domain.unit.FieldSetConfigurationUnit;
 import org.lightadmin.demo.model.Product;
 
-@SuppressWarnings( "unused" )
-@Administration( Product.class )
+@SuppressWarnings("unused")
+@Administration(Product.class)
 public class ProductAdministration {
 
 	public static EntityMetadataConfigurationUnit configuration( EntityMetadataConfigurationUnitBuilder configurationBuilder ) {
@@ -21,48 +21,26 @@ public class ProductAdministration {
 	}
 
 	public static ScreenContextConfigurationUnit screenContext( ScreenContextConfigurationUnitBuilder screenContextBuilder ) {
-		return screenContextBuilder
-			.screenName( "Products Administration" )
-			.menuName( "Products" ).build();
+		return screenContextBuilder.screenName( "Products Administration" ).menuName( "Products" ).build();
 	}
 
 	public static FieldSetConfigurationUnit listView( FieldSetConfigurationUnitBuilder fragmentBuilder ) {
-		return fragmentBuilder
-			.field( "name" ).caption( "Name" )
-			.field( "description" ).caption( "Description" )
-			.field( "price" ).caption( "Price" )
-			.field( "retired" ).caption("Out of production").build();
+		return fragmentBuilder.field( "name" ).caption( "Name" ).field( "description" ).caption( "Description" ).field( "price" ).caption( "Price" ).field( "retired" ).caption( "Out of production" ).field( "releaseDate" ).caption( "Released on" ).build();
 	}
 
 	public static FieldSetConfigurationUnit showView( final FieldSetConfigurationUnitBuilder fragmentBuilder ) {
-		return fragmentBuilder
-			.field( "name" ).caption( "Name" )
-			.field( "description" ).caption( "Description" )
-			.field( "price" ).caption( "Price" )
-			.field( "retired" ).caption("Out of production").build();
+		return fragmentBuilder.field( "name" ).caption( "Name" ).field( "description" ).caption( "Description" ).field( "price" ).caption( "Price" ).field( "retired" ).caption( "Out of production" ).field( "releaseDate" ).caption( "Released on" ).build();
 	}
 
 	public static FieldSetConfigurationUnit formView( final PersistentFieldSetConfigurationUnitBuilder fragmentBuilder ) {
-		return fragmentBuilder
-				.field( "name" ).caption( "Name" )
-				.field( "description" ).caption( "Description" )
-				.field( "price" ).caption( "Price" )
-				.field( "releaseDate" ).caption( "Released on" )
-				.field( "retired" ).caption("Out of production").build();
+		return fragmentBuilder.field( "name" ).caption( "Name" ).field( "description" ).caption( "Description" ).field( "price" ).caption( "Price" ).field( "releaseDate" ).caption( "Released on" ).field( "retired" ).caption( "Out of production" ).build();
 	}
 
 	public static FieldSetConfigurationUnit quickView( final FieldSetConfigurationUnitBuilder fragmentBuilder ) {
-		return fragmentBuilder
-			.field( "name" ).caption( "Name" )
-			.field( "description" ).caption( "Description" )
-			.field( "retired" ).caption("Out of production").build();
+		return fragmentBuilder.field( "name" ).caption( "Name" ).field( "description" ).caption( "Description" ).field( "retired" ).caption( "Out of production" ).field( "releaseDate" ).caption( "Released on" ).build();
 	}
 
 	public static FiltersConfigurationUnit filters( final FiltersConfigurationUnitBuilder filterBuilder ) {
-		return filterBuilder
-			.filter( "Name", "name" )
-			.filter( "Description", "description" )
-			.filter( "Price", "price" )
-			.filter( "Retired", "retired" ).build();
+		return filterBuilder.filter( "Name", "name" ).filter( "Description", "description" ).filter( "Price", "price" ).filter( "Retired", "retired" ).build();
 	}
 }
