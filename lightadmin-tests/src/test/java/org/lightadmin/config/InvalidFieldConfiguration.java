603bee6d378cfe4f570bf99f70e2dec768676874
ikostenko <iryna.kostenko@gmail.com>
2013-04-23 01:18:11 +0300

    #72 Test added

---
+++ lightadmin-tests/src/test/java/org/lightadmin/config/InvalidFieldConfiguration.java
@@ -0,0 +1,16 @@
+package org.lightadmin.config;
+
+import org.lightadmin.core.config.annotation.Administration;
+import org.lightadmin.core.config.domain.filter.FiltersConfigurationUnit;
+import org.lightadmin.core.config.domain.filter.FiltersConfigurationUnitBuilder;
+import org.lightadmin.test.model.TestOrder;
+
+@SuppressWarnings( "unused" )
+@Administration( TestOrder.class )
+public class InvalidFieldConfiguration {
+
+	public static FiltersConfigurationUnit filters( final FiltersConfigurationUnitBuilder filterBuilder ) {
+		return filterBuilder
+				.filter( "Missing Field", "missingField" ).build();
+	}
+}
