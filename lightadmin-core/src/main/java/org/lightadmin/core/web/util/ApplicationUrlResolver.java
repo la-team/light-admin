c99801e0a66ae37b7005099a69710e059b5d3208
max-dev <kharchenko.max@gmail.com>
2013-09-29 18:26:31 +0300

    Binary data retrieval through REST API for not authorized users => jstl user function added.

--- lightadmin-core/src/main/java/org/lightadmin/core/web/util/ApplicationUrlResolver.java
+++ lightadmin-core/src/main/java/org/lightadmin/core/web/util/ApplicationUrlResolver.java
@@ -1,25 +1,50 @@
 package org.lightadmin.core.web.util;
 
 import org.lightadmin.core.config.domain.DomainTypeAdministrationConfiguration;
+import org.lightadmin.core.persistence.metamodel.DomainTypeEntityMetadata;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.Serializable;
+
+import static java.lang.String.valueOf;
+import static org.lightadmin.core.web.util.WebContextUtils.getCurrentRequest;
+import static org.lightadmin.core.web.util.WebContextUtils.globalAdministrationConfiguration;
 
 public final class ApplicationUrlResolver {
 
-	private ApplicationUrlResolver() {
-	}
+    private ApplicationUrlResolver() {
+    }
 
-	public static String domainBaseUrl( DomainTypeAdministrationConfiguration configuration ) {
-		return "/domain/" + configuration.getDomainTypeName();
-	}
+    public static String domainBaseUrl(DomainTypeAdministrationConfiguration configuration) {
+        return "/domain/" + configuration.getDomainTypeName();
+    }
 
-	public static String domainRestBaseUrl( DomainTypeAdministrationConfiguration configuration ) {
-		return "/rest/" + configuration.getDomainTypeName();
-	}
+    public static String domainRestBaseUrl(DomainTypeAdministrationConfiguration configuration) {
+        return "/rest/" + configuration.getDomainTypeName();
+    }
 
-	public static String domainRestEntityBaseUrl( DomainTypeAdministrationConfiguration configuration, Object id ) {
-		return domainRestBaseUrl( configuration ) + "/" + id;
-	}
+    public static String domainRestEntityBaseUrl(DomainTypeAdministrationConfiguration configuration, Object id) {
+        return domainRestBaseUrl(configuration) + "/" + id;
+    }
 
-	public static String domainRestScopeBaseUrl( DomainTypeAdministrationConfiguration configuration ) {
-		return "/rest/" + configuration.getDomainTypeName() + "/scope";
-	}
+    public static String domainRestScopeBaseUrl(DomainTypeAdministrationConfiguration configuration) {
+        return "/rest/" + configuration.getDomainTypeName() + "/scope";
+    }
+
+    public static String filePropertyRestUrl(Object entity, String property) {
+        DomainTypeAdministrationConfiguration domainTypeAdministrationConfiguration = domainTypeAdministrationConfiguration(getCurrentRequest(), entity);
+        DomainTypeEntityMetadata domainTypeEntityMetadata = domainTypeAdministrationConfiguration.getDomainTypeEntityMetadata();
+
+        String idValue = valueOf(idAttributeValue(entity, domainTypeEntityMetadata));
+
+        return domainRestEntityBaseUrl(domainTypeAdministrationConfiguration, idValue) + "/" + property + "/file";
+    }
+
+    private static DomainTypeAdministrationConfiguration domainTypeAdministrationConfiguration(HttpServletRequest currentRequest, Object entity) {
+        return globalAdministrationConfiguration(currentRequest.getServletContext()).forManagedDomainType(entity.getClass());
+    }
+
+    private static Serializable idAttributeValue(Object entity, DomainTypeEntityMetadata domainTypeEntityMetadata) {
+        return (Serializable) domainTypeEntityMetadata.getIdAttribute().getValue(entity);
+    }
 }
--- lightadmin-core/src/main/java/org/lightadmin/core/web/util/WebContextUtils.java
+++ lightadmin-core/src/main/java/org/lightadmin/core/web/util/WebContextUtils.java
@@ -1,19 +1,39 @@
 package org.lightadmin.core.web.util;
 
+import org.lightadmin.core.config.domain.GlobalAdministrationConfiguration;
 import org.lightadmin.core.util.LightAdminConfigurationUtils;
+import org.springframework.util.Assert;
 import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 import org.springframework.web.context.support.WebApplicationContextUtils;
 import org.springframework.web.servlet.FrameworkServlet;
 
 import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
 
 public abstract class WebContextUtils {
 
-	public static WebApplicationContext getWebApplicationContext( ServletContext servletContext ) {
-		return WebApplicationContextUtils.getWebApplicationContext( servletContext, servletContextAttributeName() );
-	}
+    public static WebApplicationContext getWebApplicationContext(ServletContext servletContext) {
+        return WebApplicationContextUtils.getWebApplicationContext(servletContext, servletContextAttributeName());
+    }
 
-	public static String servletContextAttributeName() {
-		return FrameworkServlet.SERVLET_CONTEXT_PREFIX + LightAdminConfigurationUtils.LIGHT_ADMIN_DISPATCHER_NAME;
-	}
+    public static String servletContextAttributeName() {
+        return FrameworkServlet.SERVLET_CONTEXT_PREFIX + LightAdminConfigurationUtils.LIGHT_ADMIN_DISPATCHER_NAME;
+    }
+
+    public static GlobalAdministrationConfiguration globalAdministrationConfiguration(ServletContext servletContext) {
+        WebApplicationContext webApplicationContext = getWebApplicationContext(servletContext);
+        return webApplicationContext.getBean(GlobalAdministrationConfiguration.class);
+    }
+
+    public static HttpServletRequest getCurrentRequest() {
+        RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();
+        Assert.state(requestAttributes != null, "Could not find current request via RequestContextHolder");
+        Assert.isInstanceOf(ServletRequestAttributes.class, requestAttributes);
+        HttpServletRequest servletRequest = ((ServletRequestAttributes) requestAttributes).getRequest();
+        Assert.state(servletRequest != null, "Could not find current HttpServletRequest");
+        return servletRequest;
+    }
 }
