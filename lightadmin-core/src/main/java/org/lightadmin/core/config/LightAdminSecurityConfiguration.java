8d121ff80c3fc512df582592c56bbb07efc78011
max-dev <kharchenko.max@gmail.com>
2013-09-29 17:02:16 +0300

    Security change: binary data retrieval through REST API for not authorized users.

--- lightadmin-core/src/main/java/org/lightadmin/core/config/LightAdminSecurityConfiguration.java
+++ lightadmin-core/src/main/java/org/lightadmin/core/config/LightAdminSecurityConfiguration.java
@@ -55,134 +55,136 @@ import static java.util.Arrays.asList;
 @Configuration
 public class LightAdminSecurityConfiguration {
 
-	private static final String ROLE_ADMIN = "ROLE_ADMIN";
-	private static final String REMEMBER_ME_DIGEST_KEY = "LightAdmin";
+    private static final String ROLE_ADMIN = "ROLE_ADMIN";
+    private static final String REMEMBER_ME_DIGEST_KEY = "LightAdmin";
 
-	private static final String[] PUBLIC_RESOURCES = {
-		"/images/**", "/scripts/**", "/styles/**", "/login", "/page-not-found", "/access-denied"
-	};
+    private static final String[] PUBLIC_RESOURCES = {
+            "/images/**", "/scripts/**", "/styles/**", "/rest/**/file",
+            "/login", "/page-not-found", "/access-denied"
+    };
 
-	@Value("classpath:users.properties")
-	private Resource usersResource;
+    @Value("classpath:users.properties")
+    private Resource usersResource;
 
-	@Autowired
-	private WebContext webContext;
+    @Autowired
+    private WebContext webContext;
 
-	@Bean
-	@Autowired
-	public FilterChainProxy springSecurityFilterChain( Filter filterSecurityInterceptor, Filter authenticationFilter, Filter rememberMeAuthenticationFilter, Filter logoutFilter, Filter exceptionTranslationFilter, Filter securityContextPersistenceFilter ) {
+    @Bean
+    @Autowired
+    public FilterChainProxy springSecurityFilterChain(Filter filterSecurityInterceptor, Filter authenticationFilter, Filter rememberMeAuthenticationFilter, Filter logoutFilter, Filter exceptionTranslationFilter, Filter securityContextPersistenceFilter) {
 
-		List<SecurityFilterChain> filterChains = new ArrayList<SecurityFilterChain>();
-		for ( String pattern : PUBLIC_RESOURCES ) {
-			filterChains.add( new DefaultSecurityFilterChain( new AntPathRequestMatcher( applicationUrl(pattern) ) ) );
-		}
+        List<SecurityFilterChain> filterChains = new ArrayList<SecurityFilterChain>();
+        for (String pattern : PUBLIC_RESOURCES) {
+            filterChains.add(new DefaultSecurityFilterChain(new AntPathRequestMatcher(applicationUrl(pattern))));
+        }
 
-		filterChains.add( new DefaultSecurityFilterChain( new AnyRequestMatcher(), securityContextPersistenceFilter, exceptionTranslationFilter, logoutFilter, authenticationFilter, rememberMeAuthenticationFilter, filterSecurityInterceptor ) );
+        filterChains.add(new DefaultSecurityFilterChain(new AnyRequestMatcher(), securityContextPersistenceFilter, exceptionTranslationFilter, logoutFilter, authenticationFilter, rememberMeAuthenticationFilter, filterSecurityInterceptor));
 
-		return new FilterChainProxy( filterChains );
-	}
+        return new FilterChainProxy(filterChains);
+    }
 
-	@Bean
-	@Autowired
-	public Filter filterSecurityInterceptor( AuthenticationManager authenticationManager ) throws Exception {
-		FilterSecurityInterceptor filter = new FilterSecurityInterceptor();
-		filter.setAuthenticationManager( authenticationManager );
-		filter.setAccessDecisionManager( new AffirmativeBased( asList( ( AccessDecisionVoter ) new RoleVoter() ) ) );
-		filter.setSecurityMetadataSource( securityMetadataSource() );
-		filter.afterPropertiesSet();
-		return filter;
-	}
+    @Bean
+    @Autowired
+    public Filter filterSecurityInterceptor(AuthenticationManager authenticationManager) throws Exception {
+        FilterSecurityInterceptor filter = new FilterSecurityInterceptor();
+        filter.setAuthenticationManager(authenticationManager);
+        filter.setAccessDecisionManager(new AffirmativeBased(asList((AccessDecisionVoter) new RoleVoter())));
+        filter.setSecurityMetadataSource(securityMetadataSource());
+        filter.afterPropertiesSet();
+        return filter;
+    }
 
-	private FilterInvocationSecurityMetadataSource securityMetadataSource() {
-		LinkedHashMap<RequestMatcher, Collection<ConfigAttribute>> map = new LinkedHashMap<RequestMatcher, Collection<ConfigAttribute>>();
-		map.put( new AnyRequestMatcher(), asList( ( ConfigAttribute ) new SecurityConfig( ROLE_ADMIN ) ) );
-		return new DefaultFilterInvocationSecurityMetadataSource( map );
-	}
+    private FilterInvocationSecurityMetadataSource securityMetadataSource() {
+        LinkedHashMap<RequestMatcher, Collection<ConfigAttribute>> map = new LinkedHashMap<RequestMatcher, Collection<ConfigAttribute>>();
+        map.put(new AnyRequestMatcher(), asList((ConfigAttribute) new SecurityConfig(ROLE_ADMIN)));
+        return new DefaultFilterInvocationSecurityMetadataSource(map);
+    }
 
-	@Bean
-	@Autowired
-	public Filter authenticationFilter( AuthenticationManager authenticationManager, RequestCache requestCache ) {
-		UsernamePasswordAuthenticationFilter authenticationFilter = new UsernamePasswordAuthenticationFilter();
-		authenticationFilter.setFilterProcessesUrl( applicationUrl("/j_spring_security_check") );
-		authenticationFilter.setAuthenticationManager( authenticationManager );
-		SavedRequestAwareAuthenticationSuccessHandler successHandler = new SavedRequestAwareAuthenticationSuccessHandler();
-		successHandler.setRequestCache( requestCache );
-		authenticationFilter.setAuthenticationSuccessHandler( successHandler );
-		authenticationFilter.setAuthenticationFailureHandler( new SimpleUrlAuthenticationFailureHandler( applicationUrl("/login?login_error=1") ) );
-		return authenticationFilter;
-	}
+    @Bean
+    @Autowired
+    public Filter authenticationFilter(AuthenticationManager authenticationManager, RequestCache requestCache) {
+        UsernamePasswordAuthenticationFilter authenticationFilter = new UsernamePasswordAuthenticationFilter();
+        authenticationFilter.setFilterProcessesUrl(applicationUrl("/j_spring_security_check"));
+        authenticationFilter.setAuthenticationManager(authenticationManager);
+        SavedRequestAwareAuthenticationSuccessHandler successHandler = new SavedRequestAwareAuthenticationSuccessHandler();
+        successHandler.setRequestCache(requestCache);
+        authenticationFilter.setAuthenticationSuccessHandler(successHandler);
+        authenticationFilter.setAuthenticationFailureHandler(new SimpleUrlAuthenticationFailureHandler(applicationUrl("/login?login_error=1")));
+        return authenticationFilter;
+    }
 
-	@Bean
-	public Filter exceptionTranslationFilter( RequestCache requestCache ) {
-		AccessDeniedHandlerImpl accessDeniedHandler = new AccessDeniedHandlerImpl();
-		accessDeniedHandler.setErrorPage( applicationUrl("/access-denied") );
-		LoginUrlAuthenticationEntryPoint authenticationEntryPoint = new LoginUrlAuthenticationEntryPoint( applicationUrl("/login") );
-		ExceptionTranslationFilter exceptionTranslationFilter = new ExceptionTranslationFilter( authenticationEntryPoint, requestCache );
-		exceptionTranslationFilter.setAccessDeniedHandler( accessDeniedHandler );
-		return exceptionTranslationFilter;
-	}
+    @Bean
+    public Filter exceptionTranslationFilter(RequestCache requestCache) {
+        AccessDeniedHandlerImpl accessDeniedHandler = new AccessDeniedHandlerImpl();
+        accessDeniedHandler.setErrorPage(applicationUrl("/access-denied"));
+        LoginUrlAuthenticationEntryPoint authenticationEntryPoint = new LoginUrlAuthenticationEntryPoint(applicationUrl("/login"));
+        ExceptionTranslationFilter exceptionTranslationFilter = new ExceptionTranslationFilter(authenticationEntryPoint, requestCache);
+        exceptionTranslationFilter.setAccessDeniedHandler(accessDeniedHandler);
+        return exceptionTranslationFilter;
+    }
 
-	@Bean
-	public Filter logoutFilter() {
-		SecurityContextLogoutHandler logoutHandler = new SecurityContextLogoutHandler();
-		logoutHandler.setInvalidateHttpSession( false );
-		LogoutFilter logoutFilter = new LogoutFilter( applicationUrl("/"), logoutHandler );
-		logoutFilter.setFilterProcessesUrl( applicationUrl("/logout") );
-		return logoutFilter;
-	}
+    @Bean
+    public Filter logoutFilter() {
+        SecurityContextLogoutHandler logoutHandler = new SecurityContextLogoutHandler();
+        logoutHandler.setInvalidateHttpSession(false);
+        LogoutFilter logoutFilter = new LogoutFilter(applicationUrl("/"), logoutHandler);
+        logoutFilter.setFilterProcessesUrl(applicationUrl("/logout"));
+        return logoutFilter;
+    }
 
-	@Bean
-	public Filter securityContextPersistenceFilter() {
-		HttpSessionSecurityContextRepository repo = new HttpSessionSecurityContextRepository();
-		repo.setSpringSecurityContextKey( keyWithNamespace( HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY ) );
-		return new SecurityContextPersistenceFilter( repo );
-	}
+    @Bean
+    public Filter securityContextPersistenceFilter() {
+        HttpSessionSecurityContextRepository repo = new HttpSessionSecurityContextRepository();
+        repo.setSpringSecurityContextKey(keyWithNamespace(HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY));
+        return new SecurityContextPersistenceFilter(repo);
+    }
 
-	@Bean
-	public Filter rememberMeAuthenticationFilter( AuthenticationManager authenticationManager, UserDetailsService userDetailsService ) {
-		TokenBasedRememberMeServices rememberMeServices = new TokenBasedRememberMeServices( REMEMBER_ME_DIGEST_KEY, userDetailsService );
-		rememberMeServices.setCookieName( keyWithNamespace( AbstractRememberMeServices.SPRING_SECURITY_REMEMBER_ME_COOKIE_KEY ) );
-		return new RememberMeAuthenticationFilter( authenticationManager, rememberMeServices );
-	}
+    @Bean
+    public Filter rememberMeAuthenticationFilter(AuthenticationManager authenticationManager, UserDetailsService userDetailsService) {
+        TokenBasedRememberMeServices rememberMeServices = new TokenBasedRememberMeServices(REMEMBER_ME_DIGEST_KEY, userDetailsService);
+        rememberMeServices.setCookieName(keyWithNamespace(AbstractRememberMeServices.SPRING_SECURITY_REMEMBER_ME_COOKIE_KEY));
+        return new RememberMeAuthenticationFilter(authenticationManager, rememberMeServices);
+    }
 
-	@Bean
-	public RequestCache requestCache() {
-		return new LightAdminRequestCache();
-	}
+    @Bean
+    public RequestCache requestCache() {
+        return new LightAdminRequestCache();
+    }
 
-	@Bean
-	@Autowired
-	public AuthenticationManager authenticationManager( AuthenticationProvider authenticationProvider, AuthenticationProvider rememberMeAuthenticationProvider ) {
-		return new ProviderManager( asList( authenticationProvider, rememberMeAuthenticationProvider ) );
-	}
+    @Bean
+    @Autowired
+    public AuthenticationManager authenticationManager(AuthenticationProvider authenticationProvider, AuthenticationProvider rememberMeAuthenticationProvider) {
+        return new ProviderManager(asList(authenticationProvider, rememberMeAuthenticationProvider));
+    }
 
-	@Bean
-	@Autowired
-	public AuthenticationProvider authenticationProvider( UserDetailsService usersService ) throws Exception {
-		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-		provider.setPasswordEncoder( new ShaPasswordEncoder() );
-		provider.setUserDetailsService( usersService );
-		provider.afterPropertiesSet();
-		return provider;
-	}
+    @Bean
+    @Autowired
+    public AuthenticationProvider authenticationProvider(UserDetailsService usersService) throws Exception {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(new ShaPasswordEncoder());
+        provider.setUserDetailsService(usersService);
+        provider.afterPropertiesSet();
+        return provider;
+    }
 
-	@Bean @Primary
-	public UserDetailsService userDetailsService() throws IOException {
-		Properties usersPproperties = PropertiesLoaderUtils.loadProperties( usersResource );
-		return new InMemoryUserDetailsManager( usersPproperties );
-	}
+    @Bean
+    @Primary
+    public UserDetailsService userDetailsService() throws IOException {
+        Properties usersPproperties = PropertiesLoaderUtils.loadProperties(usersResource);
+        return new InMemoryUserDetailsManager(usersPproperties);
+    }
 
-	@Bean
-	public AuthenticationProvider rememberMeAuthenticationProvider() {
-		return new RememberMeAuthenticationProvider( REMEMBER_ME_DIGEST_KEY );
-	}
+    @Bean
+    public AuthenticationProvider rememberMeAuthenticationProvider() {
+        return new RememberMeAuthenticationProvider(REMEMBER_ME_DIGEST_KEY);
+    }
 
-	private String applicationUrl( String path ) {
-		return webContext.getApplicationUrl( path );
-	}
+    private String applicationUrl(String path) {
+        return webContext.getApplicationUrl(path);
+    }
 
-	private String keyWithNamespace( String key ) {
-		return "lightadmin:" + key;
-	}
+    private String keyWithNamespace(String key) {
+        return "lightadmin:" + key;
+    }
 
 }
